3ec91f06a29a5c70d612a0e88fdb1a1390c53375
Matthew Karle <mkarle2178@gmail.com>
2019-03-31 10:48:47 -0400

    refactored for user input

--- src/GraphicsPanel.java
+++ src/GraphicsPanel.java
@@ -1,16 +1,49 @@
 import javax.swing.*;
+import javax.swing.Timer;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class GraphicsPanel extends JPanel {
 
-    private int generation = 0 , score = 100000;
-    private JPanel doctorsPanel;
+    private int generation = 0 , score = 100000, w, h;
+    private GregorianCalendar calendar;
+    private enum State {
+        START, ACTIVE
+    }
+    private State curr_state;
     public GraphicsPanel(int w, int h){
         super(null);
         setSize(w, h);
+        this.w = w;
+        this.h = h;
 
+        setState(State.START);
+    }
+
+    public void setGeneration(int generation) {
+        this.generation = generation;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    private void addDoctors(int w, int h){
         //doctorsPanel
-        doctorsPanel = new JPanel(null);
+        JPanel doctorsPanel = new JPanel(null);
         add(doctorsPanel);
         doctorsPanel.setBounds(0, 40, w/5, h - 80);
         doctorsPanel.setVisible(true);
@@ -27,21 +60,213 @@ public class GraphicsPanel extends JPanel {
             button.setBounds(0, count * height + 10, w/5, height);
             count++;
         }
+
+        //guarantee buttons visible
         doctorsPanel.revalidate();
         doctorsPanel.repaint();
     }
 
-    public void paintComponent(Graphics g){
-        Graphics2D g2 = (Graphics2D)g;
-        g2.drawString("Generation: " + generation, 0, 20);
-        g2.drawString("Score: " + score, 0, 40);
+    private void setState(State state){
+        removeAll();
+        curr_state = state;
+        switch(state){
+            case START:
+                //Start Screen
+                buildStartScreen(w, h);
+                break;
+            case ACTIVE:
+                //Active Screen
+                buildActiveScreen(w, h);
+                break;
+        }
+        revalidate();
+        repaint();
     }
 
-    public void setGeneration(int generation) {
-        this.generation = generation;
+    private void buildStartScreen(int w, int h){
+        //add title
+        JLabel title = new JLabel("Call Scheduler");
+        title.setHorizontalAlignment(SwingConstants.CENTER);
+        title.setFont(new Font("TimesRoman", Font.BOLD, 76));
+        title.setBounds(50, h/2 - 150, w - 100, h/10);
+        add(title);
+
+        //add file choosing button
+        JFileChooser fileChooser = new JFileChooser();
+        JButton fileButton = new JButton("Choose File");
+        fileButton.setBounds(w/2 - 100, h/2, 200, 50);
+        add(fileButton);
+        fileButton.addActionListener(e -> { //choose file functionality
+            //filter results to txt or csv only
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("Readable Text Files", "txt", "csv");
+            fileChooser.setFileFilter(filter);
+
+            //open choosing window
+            int returnVal = fileChooser.showOpenDialog(this);
+            if(returnVal == JFileChooser.APPROVE_OPTION){
+                fileButton.setText(fileChooser.getSelectedFile().getName());
+            }
+        });
+
+        /*----------------------------Add Week Fields------------------------*/
+
+        ArrayList<JTextField> dateInputs = new ArrayList<>(); //store fields for later access
+
+        //create container element for ease of positioning
+        JPanel container = new JPanel();
+        container.setLayout(null);
+        container.setBounds(w/2 - 150, h/2 + 100, 300, 50);
+
+        //add initial label to container
+        JLabel week = new JLabel("Starts the week of: ");
+        week.setBounds(10, 10, 150, 25);
+        container.add(week);
+
+        //add number input fields
+        for (int i = 0; i < 3; i++) {
+            //number input
+            JTextField input = new JTextField();
+            input.setBounds( week.getWidth() + 50 * i, 5, 35, 35);
+            container.add(input);
+            dateInputs.add(input);
+
+            //separator labels
+            if(i < 2){
+                JLabel separator = new JLabel("/");
+                separator.setFont(new Font("TimesRoman", Font.PLAIN, 36));
+                separator.setBounds( week.getWidth() + 50 * (i+1) - 10, 7, 25, 35);
+                container.add(separator);
+            }
+
+            else{
+                Rectangle r = input.getBounds();
+                r.width += 15;
+                input.setBounds(r);
+            }
+        }
+
+        //add container
+        add(container);
+
+        /*------------------Add Error Label-------------------*/
+        JLabel error = new JLabel();
+        error.setBounds(h/2 - 200, container.getY() + 75, 400, 50);
+        error.setHorizontalAlignment(SwingConstants.CENTER);
+        add(error);
+
+        /*------------------Add Go Button---------------------*/
+        JButton go = new JButton("Schedule!");
+        go.setFont(new Font("TimesRoman", Font.BOLD, 48));
+        go.setBounds(h/2 - 200, container.getY() + 150, 400, 100);
+        add(go);
+        go.addActionListener(e -> {
+            error.setText("");
+            //input checks
+            try{
+                //create calendar with given date
+                 int[] date = new int[3];
+                 for (int i = 0; i < 3; i++) {
+                     String text = dateInputs.get(i).getText();
+                     if(i == 2 && text.length() < 4){
+                         text = "20" + text;
+                     }
+                    date[i] = Integer.parseInt(text);
+                 }
+                 calendar = new GregorianCalendar(date[2], date[0] - 1, date[1]);
+                 System.out.println(calendar.getTime());
+
+                 //read input file for doctor schedules
+                File schedule = fileChooser.getSelectedFile();
+                readFile(schedule);
+
+                //Activate algorithm
+                Main.startTimer();
+                setState(State.ACTIVE);
+            }
+            catch(Exception exception){
+                exception.printStackTrace();
+                error.setText("Input is incorrect or incomplete");
+            }
+        });
+
     }
 
-    public void setScore(int score) {
-        this.score = score;
+    private void buildActiveScreen(int w, int h){
+        //add doctor panel
+        addDoctors(w, h);
+
+        //add preliminary genAlg display panel
+        JPanel genPanel = new JPanel(){
+            @Override
+            public void paintComponent(Graphics g){
+                Graphics2D g2 = (Graphics2D)g;
+                g2.drawString("Generation: " + generation, 0, 20);
+                g2.drawString("Score: " + score, 0, 40);
+            }
+        };
+        genPanel.setBounds(w - 200, 20, 180, 40);
+        add(genPanel);
+
     }
+
+    private void readFile(File file) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        HashMap<String, ArrayList<Integer>> doctors = new HashMap<>(); //temp list of doctors and constraints
+        String st = br.readLine();//first line is doctor names
+        String[] list = st.split(",");
+        for(String s : list){doctors.put(s, new ArrayList<>());}
+
+        HashMap<Integer, String> tempRequired = new HashMap<>();
+
+        /*---------------------Loop over Dates------------------*/
+        while((st = br.readLine()) != null) {
+            String[] dates = st.split(","); //split on values
+            for (int i = 0; i < dates.length; i++) {
+                String[] value = dates[i].split("-"); //is it a constraint or a request?
+                if (value.length > 1) { //constraint
+                    doctors.get(list[i]).add(interpretDate(value[1])); //add week integer to doctor constraint arraylist
+                } else { //request
+                    tempRequired.put(interpretDate(value[0]), list[i]);
+                }
+            }
+        }
+
+        for(String s : doctors.keySet()){
+            new Participant(s, doctors.get(s));
+        }
+
+        for(Integer i : tempRequired.keySet()){
+            Schedule.requiredDocs.put(i, Schedule.participantHashMap.get(tempRequired.get(i)));
+        }
+
+        Schedule.avgNumCalls = Schedule.numWeeks/Schedule.participantHashMap.keySet().size();
+
+
+    }
+
+    private int interpretDate(String date) throws Exception{
+
+        /*-----------------Correct Format---------------*/
+        String[] vals = date.split("/");
+        for (int i = 0; i < 2; i++) {
+            if(vals[i].length() < 2)
+                vals[i] = "0"+vals[i];
+        }
+        if(vals[2].length() == 2)
+            vals[2] = "20" + vals[2];
+
+        date = "";
+        for (int i = 0; i < 2; i++) {
+            date += vals[i] + "/";
+        }
+        date += vals[2];
+        /*---------------Get Week Value---------------------*/
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy", Locale.US);
+        Date d1 = sdf.parse(date);
+        long diffInMillies = Math.abs(calendar.getTime().getTime() - d1.getTime());
+        long diff = TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
+        return ((int)diff)/7;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -16,46 +16,65 @@ public class Main extends JFrame {
         setVisible(true);
         panel = new GraphicsPanel(width, height);
         add(panel);
+        panel.repaint();
 
     }
     public static void main(String[] args) {
         Schedule.numWeeks = 26;
-        int numDocs = 11;
-        ArrayList<Integer> temp = new ArrayList<>();
-        HashMap<Integer, Integer> tempRequiredList = new HashMap<>();
-        HashMap<Integer, Integer> reversedRequiredList = new HashMap<>(); //lazy way of avoiding for loops
-        HashMap<Integer, Participant> requiredList = new HashMap<>();
-        for (int i = 0; i < Schedule.numWeeks; i++) {
-            temp.add(i);
-        }
-        for (int i = 0; i < 4; i++) {
-            int docNum = (int)(Math.random() * numDocs), weekNum = temp.remove((int)(Math.random() * temp.size()));
-            tempRequiredList.put(docNum, weekNum);
-            reversedRequiredList.put(weekNum, docNum);
-        }
-        for (int i = 0; i < numDocs; i++) {
-            ArrayList<Integer> constraints = new ArrayList<>();
-            ArrayList<Integer> poss = new ArrayList<>();
-            for (int j = 0; j < Schedule.numWeeks; j++) {
-                if(reversedRequiredList.get(j) == null)
-                    poss.add(j);
-            }
-            for (int j = 0; j < 15; j++) {
-                constraints.add(poss.remove((int)(Math.random() * poss.size())));
-            }
-            Participant p = new Participant("Doctor" + i, constraints);
-            if(tempRequiredList.get(i)!=null)
-                requiredList.put(tempRequiredList.get(i), p);
-        }
-        Schedule.requiredDocs = requiredList;
-        Schedule.avgNumCalls = Schedule.numWeeks/Schedule.participantHashMap.keySet().size();
-        schedules = new Population(200);
-        System.out.print("Required Dates: ");
-        for(Integer i : tempRequiredList.keySet()){
-            System.out.print("Doctor" + i + ", " + tempRequiredList.get(i) + "; ");
-        }
+//        int numDocs = 11;
+//        ArrayList<Integer> temp = new ArrayList<>();
+//        HashMap<Integer, Integer> tempRequiredList = new HashMap<>();
+//        HashMap<Integer, Integer> reversedRequiredList = new HashMap<>(); //lazy way of avoiding for loops
+//        HashMap<Integer, Participant> requiredList = new HashMap<>();
+//        for (int i = 0; i < Schedule.numWeeks; i++) {
+//            temp.add(i);
+//        }
+//        for (int i = 0; i < 4; i++) {
+//            int docNum = (int)(Math.random() * numDocs), weekNum = temp.remove((int)(Math.random() * temp.size()));
+//            tempRequiredList.put(docNum, weekNum);
+//            reversedRequiredList.put(weekNum, docNum);
+//        }
+//        for (int i = 0; i < numDocs; i++) {
+//            ArrayList<Integer> constraints = new ArrayList<>();
+//            ArrayList<Integer> poss = new ArrayList<>();
+//            for (int j = 0; j < Schedule.numWeeks; j++) {
+//                if(reversedRequiredList.get(j) == null)
+//                    poss.add(j);
+//            }
+//            for (int j = 0; j < 15; j++) {
+//                constraints.add(poss.remove((int)(Math.random() * poss.size())));
+//            }
+//            Participant p = new Participant("Doctor" + i, constraints);
+//            if(tempRequiredList.get(i)!=null)
+//                requiredList.put(tempRequiredList.get(i), p);
+//        }
+//        Schedule.requiredDocs = requiredList;
+//        Schedule.avgNumCalls = Schedule.numWeeks/Schedule.participantHashMap.keySet().size();
+//        schedules = new Population(200);
+//        System.out.print("Required Dates: ");
+//        for(Integer i : tempRequiredList.keySet()){
+//            System.out.print("Doctor" + i + ", " + tempRequiredList.get(i) + "; ");
+//        }
         new Main();
+//        schedules.setPanel(panel);
+//        t = new Timer(20, new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                if(schedules.getNoChange() > 500 || schedules.getLastCost() == 200) {
+//                    schedules.printBestSchedule();
+//                    t.stop();
+//                }
+//                else
+//                    schedules.nextGen();
+//            }
+//        });
+//        t.start();
+    }
+
+    public static void startTimer(){
+        schedules = new Population(200);
         schedules.setPanel(panel);
+
         t = new Timer(20, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -69,5 +88,6 @@ public class Main extends JFrame {
         });
         t.start();
     }
+
 
 }
